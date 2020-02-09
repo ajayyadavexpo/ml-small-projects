7de51293f21c1186c7c37d975d987d752afe1485
AjayYadavAi <proajayyadav0525@gmail.com>
2020-02-09 18:18:42 +0530

    'added'

---
+++ check-modules.py
@@ -0,0 +1,17 @@
+# Check the versions of libraries
+ 
+# Python version
+import sys,numpy,matplotlib,pandas,sklearn,scipy
+print('Python: {}'.format(sys.version))
+# 
+# import scipy
+print('scipy: {}'.format(scipy.__version__))
+# numpy
+
+print('numpy: {}'.format(numpy.__version__))
+# matplotlib
+print('matplotlib: {}'.format(matplotlib.__version__))
+# pandas
+print('pandas: {}'.format(pandas.__version__))
+# scikit-learn
+print('sklearn: {}'.format(sklearn.__version__))
